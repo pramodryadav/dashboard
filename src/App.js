a3342e84ef28151bb307c9eb61a1bde768efc126
pramodyadav <95114706+pramodryadav@users.noreply.github.com>
2023-12-10 22:56:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import ReactCrop from 'react-image-crop';
 import 'react-image-crop/dist/ReactCrop.css';
 import CropperDemo from './components/cropper';
-import Useref from './components/UseRef';
+
 
 function App() {
 
@@ -12,7 +12,7 @@ return (
 	<div className="App">
 		
 <CropperDemo/>
-<Useref/>
+
 	</div>
 );
 }
