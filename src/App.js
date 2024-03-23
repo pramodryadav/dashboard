50990f1a2aef6c57f3b6f26fba0785e9dfa8c9ec
pramodyadav <pramodryadavall@gmail.com>
2024-03-23 22:38:02 +0530

    removed unneccessary files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import { useState } from 'react';
-import ReactCrop from 'react-image-crop';
+
 import 'react-image-crop/dist/ReactCrop.css';
 import CropperDemo from './components/cropper';
 
 
 function App() {
 
+	return (
+		<div className="App">
 
+			<CropperDemo />
 
-return (
-	<div className="App">
-		
-<CropperDemo/>
-
-	</div>
-);
+		</div>
+	);
 }
 
 export default App;
